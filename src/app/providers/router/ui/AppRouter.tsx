b41640a6599adf7038ec240c29efd5f01ca27997
FatB0YY <rodion.RV111@icloud.com>
2023-09-30 18:12:41 +0300

    Added Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,23 @@
-import { AboutPageAsync } from "pages/AboutPage";
-import { MainPageAsync } from "pages/MainPage";
-import React, { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { routeConfig } from "../config/routeConfig";
+import { AboutPageAsync } from 'pages/AboutPage'
+import { MainPageAsync } from 'pages/MainPage'
+import React, { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { routeConfig } from '../config/routeConfig'
 
 const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
-          <Route key={path} path={path} element={element} />
+          <Route
+            key={path}
+            path={path}
+            element={<div className='page-wrapper '>{element}</div>}
+          />
         ))}
       </Routes>
     </Suspense>
-  );
-};
+  )
+}
 
-export default AppRouter;
+export default AppRouter
