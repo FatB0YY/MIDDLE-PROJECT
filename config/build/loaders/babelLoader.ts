0ac01300c315254d65549048249a5ebf7ef04994
FatB0YY <rodion.RV111@icloud.com>
2023-11-15 14:33:37 +0300

    add config/build configuration webpack

---
+++ config/build/loaders/babelLoader.ts
@@ -0,0 +1,44 @@
+import { IBuildOptions } from '../types/config'
+import { removeDataTestIdBabelPlugin } from './removeDataTestIdBabelPlugin'
+
+export function buildBabelLoader({ isDev, isProd }: IBuildOptions) {
+  const plugins: any = [
+    [
+      'i18next-extract',
+      {
+        locales: ['ru', 'en'],
+        keyAsDefaultValue: true,
+      },
+    ],
+  ]
+
+  if (isProd) {
+    plugins.push([
+      removeDataTestIdBabelPlugin,
+      {
+        props: ['data-testid'],
+      },
+    ])
+  }
+
+  return {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-typescript',
+          [
+            '@babel/preset-react',
+            {
+              runtime: isDev ? 'automatic' : 'classic',
+            },
+          ],
+        ],
+        plugins: plugins,
+      },
+    },
+  }
+}
